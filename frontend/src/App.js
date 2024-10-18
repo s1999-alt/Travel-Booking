ab8f36b5c5735b64821068fb2976cecae57fcc00
Siyad Savad <siyadsavad313@gmail.com>
2024-10-19 01:39:17 +0530

    Header added and makes responsive + Swiper added and make it responsive, next is memmories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
 import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Header/>
     </div>
   );
 }
