d4e013504689c1728dd34a048d75026373b56a4f
Siyad Savad <siyadsavad313@gmail.com>
2024-10-22 17:26:04 +0530

    backend start with  env created, activate, installed django + create project TripTrails, create user and admin app, urls added, created models such as Category, Inclusions, Exclusions, Hotels, Continent, Destination, Packages, PackageImages, Itinarary for the operations of package + add all in the admin.py + created a superuser with my name + apply migrations and migrate

---
+++ backend/TripTrails/admin/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Inclusions, Exclusions, Hotels, Continent, Destination, Packages, PackageImages, Itinarary
+
+admin.site.register(Category)
+admin.site.register(Inclusions)
+admin.site.register(Exclusions)
+admin.site.register(Hotels)
+admin.site.register(Continent)
+admin.site.register(Destination)
+admin.site.register(Packages)
+admin.site.register(PackageImages)
+admin.site.register(Itinarary)
---
+++ backend/TripTrails/admin/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+
+
+class Category(models.Model):
+  category_name = models.CharField( max_length=50, unique=True )
+  is_available = models.BooleanField(default=True)
+  soft_deleted = models.BooleanField(default=False)
+  category_image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+
+  def __str__(self):
+    return self.category_name
+  
+
+class Inclusions(models.Model):
+  inclusion = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.inclusion
+  
+
+class Exclusions(models.Model):
+  exclusion = models.CharField(max_length=100) 
+
+  def __str__(self):
+    return self.exclusion
+
+
+class Hotels(models.Model):
+  hotel_name = models.CharField(max_length=50)
+  place = models.CharField(max_length=50)
+  hotel_overview = models.TextField()
+  hotel_image = models.ImageField(upload_to="hotel_images/", default="",null=True, blank=True)
+  is_available = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.hotel_name
+  
+
+class Continent(models.Model):
+  continent_name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.continent_name
+
+
+class Destination(models.Model):
+  destination_name = models.CharField(max_length=50)
+  country = models.CharField(max_length=50)
+  continent = models.ForeignKey(Continent, on_delete=models.CASCADE)
+  description = models.TextField()
+  image = models.ImageField(upload_to='package_images/', blank=True, null=True)
+
+
+class Packages(models.Model):
+  package_name = models.CharField(max_length=50, unique=True)
+  duration = models.CharField(max_length=50)
+  price = models.DecimalField(max_digits=10, decimal_places=2) 
+  sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+  overview = models.TextField()
+  category = models.ForeignKey(Category,on_delete=models.CASCADE)
+  continent = models.ForeignKey(Continent, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to='package_images/', blank=True, null=True)
+  city = models.CharField(max_length=50)
+  country = models.CharField(max_length=50,blank=True)
+  rating = models.DecimalField(max_digits=5, decimal_places=2)
+  inclusions = models.ManyToManyField(Inclusions, blank=True)
+  exclusions = models.ManyToManyField(Exclusions, blank=True)
+  hotels = models.ManyToManyField(Hotels, blank=True)
+  is_active = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.package_name
+  
+
+class PackageImages(models.Model):
+  package = models.ForeignKey("app.Model", related_name='images', on_delete=models.CASCADE)
+  image = models.ImageField(upload_to="package_images/", default="", null=True, blank=True)
+
+
+class Itinarary(models.Model):
+  package = models.ForeignKey(Packages , on_delete=models.CASCADE)
+  day_number = models.PositiveIntegerField()
+  activities = models.TextField()
+
+  def __str__(self):
+    return f"{self.package.package_name} - Day{self.day_number}"
+
+
+
+
